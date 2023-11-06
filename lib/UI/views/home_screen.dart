8c33307aca1d9a897d8fd43d8901f5e3672fc009
Moin97 <moinuddin9777@gmail.com>
2023-11-06 11:38:22 +0530

    Added responsiveness

--- lib/UI/views/home_screen.dart
+++ lib/UI/views/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:widget_vault/UI/widgets/brand_button.dart';
-import 'package:widget_vault/brand_model.dart';
+// import 'package:widget_vault/UI/widgets/brand_button.dart';
+import 'package:widget_vault/UI/widgets/brand_card.dart';
+// import 'package:widget_vault/brand_model.dart';
 import 'package:widget_vault/placeholder_data.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,18 +11,106 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+/// RESPOSIVENESS
+///
+/// Layout Builder
+/// MediaQuery
+/// plugin
+///
+
 class _HomeScreenState extends State<HomeScreen> {
   List<Brand> brandList = brandsData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: brandList.length,
-          itemBuilder: (context, index) {
-            return BrandButton(
-                brandName: brandList[index].title,
-                imageUrl: brandList[index].imageUrl);
-          }),
+        body: Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: LayoutBuilder(builder: (context, constraints) {
+          if (constraints.maxWidth > 400) {
+            // print("*************************************");
+            // print(MediaQuery.of(context).size.width);
+            // print("*************************************");
+            return const TabGrid();
+          } else {
+            return const MobileGrid();
+          }
+        }),
+      ),
+    ));
+  }
+}
+
+class TabGrid extends StatelessWidget {
+  const TabGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Brand> brandList = brandsData;
+    return Column(
+      children: [
+        Row(
+          children: [
+            Card(
+              color: Colors.amber,
+              child: Container(
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Text(
+                "This is some text that goes along with the, This is some text that goes along with the , This is some text that goes along with the  ")
+          ],
+        ),
+        Expanded(
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6),
+              itemCount: brandList.length,
+              itemBuilder: (context, index) {
+                return BrandCard(
+                    // brandName: brandList[index].title,
+                    assetLink: brandList[index].imageUrl);
+              }),
+        ),
+      ],
+    );
+  }
+}
+
+class MobileGrid extends StatelessWidget {
+  const MobileGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Brand> brandList = brandsData;
+    return Column(
+      children: [
+        Column(
+          children: [
+            Card(
+              color: Colors.amber,
+              child: Container(
+                width: 200,
+                height: 100,
+              ),
+            ),
+            Text(
+                "This is some text that goes along with the, This is some text that goes along with the , This is some text that goes along with the  ")
+          ],
+        ),
+        Expanded(
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemCount: brandList.length,
+              itemBuilder: (context, index) {
+                return BrandCard(
+                    // brandName: brandList[index].title,
+                    assetLink: brandList[index].imageUrl);
+              }),
+        ),
+      ],
     );
   }
 }
