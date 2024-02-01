ebc2eb13ea1efc8d02e94105800985f8d5ae4bbe
Thanoj <126764120+thanu2932@users.noreply.github.com>
2024-02-01 16:53:53 +0530

    Update practice_services.dart

--- lib/services/practice_services.dart
+++ lib/services/practice_services.dart
@@ -1,63 +1,33 @@
-// ignore_for_file: avoid_print
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:widget_vault/services/practice_services.dart';
 
-import 'dart:convert';
+class YourWidget extends StatelessWidget {
+  final PracticeServices practiceServices = Get.put(PracticeServices());
 
-import 'package:dio/dio.dart';
-import 'package:widget_vault/core/app_constants.dart';
-import 'package:widget_vault/models/responses/posts_response.dart';
-
-class PracticeServices {
-  // globally we'll define api url
-  // write a future method assign reponse model to it
-  // save the data
-
-  static Future<dynamic> fetchPosts() async {
-    // dio instance
-    final dio = Dio();
-
-    // here is our request
-    try {
-      final response = await dio.get("${AppConstants.API_URL}/posts");
-      if (response.statusCode == 200) {
-        final data = response.data;
-        print(data);
-        return data;
-      } else {
-        throw Exception("API CALL HAS FAILED");
-      }
-    } on DioException catch (error) {
-      print("++++++++++++++++++++++++");
-      print(error.message);
-      print("++++++++++++++++++++++++");
-      return null;
-    }
-  }
-
-  static Future<PostsResponse> fetchPostsData() async {
-    // dio instance
-    final dio = Dio();
-
-    // here is our request
-    try {
-      final response = await dio.get("${AppConstants.API_URL}/posts");
-      if (response.statusCode == 200) {
-        final data = response.data;
-        print("++++++++++ JSON DATA +++++++++++++++");
-        print(data);
-        print("++++++++++++++++++++++++++++++++");
-        final List<Post> modelData =
-            List<Post>.from(response.data.map((x) => Post.fromJson(x)));
-        // return PostsResponse(
-        //     success: true, message: "Success", posts: modelData);
-        return data;
-      } else {
-        throw Exception("API CALL HAS FAILED");
-      }
-    } on DioException catch (error) {
-      print("++++++++++++++++++++++++");
-      print(error.message);
-      print("++++++++++++++++++++++++");
-      return PostsResponse(success: false, message: error.message);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Widget'),
+      ),
+      body: Center(
+        child: Obx(
+          () => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (Post post in practiceServices.postsList)
+                Text(post.title),
+              ElevatedButton(
+                onPressed: () async {
+                  await practiceServices.fetchPosts();
+                },
+                child: Text('Fetch Posts'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
