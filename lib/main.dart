460e9af1425692c638c1cddb9be596d639f12db7
Thanoj <126764120+thanu2932@users.noreply.github.com>
2024-02-01 16:11:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:widget_vault/UI/views/home_screen.dart';
+import 'package:widget_vault/views/front_page.dart';
+import 'package:widget_vault/views/next_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-// While using GetX as state management
-// Step 1: Add package
-// Step 2: change the MaterialApp method
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Your Updated App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
+        accentColor: Colors.orange,
+        fontFamily: 'Roboto',
       ),
-      initialBinding: HomeBinding(),
-      home: const HomeScreen(),
-      // home: FormWithValidation(),
+      initialRoute: '/first',
+      getPages: [
+        GetPage(
+          name: '/first',
+          page: () => FirstPage(),
+        ),
+        GetPage(
+          name: '/second',
+          page: () => SecondPage(),
+        ),
+      ],
     );
   }
 }
-
-// While using Riverpod as state management
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const HomeScreen(),
-//     );
-//   }
-// }
