10921c1989dec7fb6ff7a5ea0e91ac913eb99e16
Thanoj <126764120+thanu2932@users.noreply.github.com>
2024-02-01 16:42:09 +0530

    Update back_page.dart

--- lib/UI/views/products_screen.dart
+++ lib/UI/views/products_screen.dart
@@ -1,119 +1,114 @@
+import 'package:flutter/material.dart';
 import 'dart:io';
 
-import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-import 'package:widget_vault/controllers/form_controller.dart';
-// import 'package:flutter_svg/svg.dart';
-// import 'package:widget_vault/UI/views/brand_details_page.dart';
-// import 'package:widget_vault/core/app_images.dart';
-import 'package:widget_vault/placeholder_data.dart';
 import 'package:get/get.dart';
 
-class ProductsPage extends StatefulWidget {
-  const ProductsPage({required this.pageName, super.key});
-
-  final String pageName;
-
-  @override
-  State<ProductsPage> createState() => _ProductsPageState();
-}
-
-class _ProductsPageState extends State<ProductsPage> {
-  // XFile imageArgument = Get.arguments['pickedImage'];
-  List<Category> listOfCategories = categoriesData;
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Map<String, dynamic>? arguments =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+
+    if (arguments == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Text('Error: Invalid data passed to the second page'),
+        ),
+      );
+    }
+
+    final String name = arguments['name'] ?? '';
+    final String imagePath = arguments['imagePath'] ?? '';
+    final String location = arguments['location'] ?? '';
+
+    final List<String> locationParts = location.split(',');
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightBlueAccent,
-        title: Text(widget.pageName),
+        title: Text('Information Details'),
+        backgroundColor: Colors.green,
       ),
-      body: GetBuilder<FormController>(
-        builder: (formInstance) {
-          return Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: CustomScrollView(
-              slivers: [
-                SliverList.builder(
-                    itemCount: 1,
-                    itemBuilder: (context, index) {
-                      return SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Image.file(
-                            File(formInstance.imagesList[index].path)),
-                      );
-                      // CategoryTile(
-                      //     brandName: listOfCategories[index].name,
-                      //     listOfBrands: listOfCategories[index].brandsList,
-                      //     categoryId: listOfCategories[index].catId);
-                    })
-              ],
-            ),
-          );
-        },
-      ),
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   crossAxisAlignment: CrossAxisAlignment.center,
-      //   children: [
-      //     SizedBox(width: double.infinity),
-      //     CategoryTile(brandName: brandName, listOfBrands: listOfBrands, categoryId: categoryId)
-      //     // Text('PRODUCTS PAGE'),
-      //     // ElevatedButton(
-      //     //     onPressed: () {
-      //     //       Navigator.of(context).push(MaterialPageRoute(
-      //     //           builder: (context) => BrandDetails(
-      //     //               brandId: "id1234",
-      //     //               brandName: "Maybelline",
-      //     //               imageUrl: "imageUrl")));
-      //     //     },
-      //     //     child: Text('Go to brand'))
-      //   ],
-      // ),
-    );
-  }
-}
-
-class CategoryTile extends StatefulWidget {
-  const CategoryTile(
-      {required this.brandName,
-      required this.listOfBrands,
-      required this.categoryId,
-      super.key});
-
-  final List<Brand> listOfBrands;
-  final int categoryId;
-  final String brandName;
-
-  @override
-  State<CategoryTile> createState() => _CategoryTileState();
-}
-
-class _CategoryTileState extends State<CategoryTile> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        child: ElevatedButton(
-          onPressed: () {},
-          // width: 40,
-          // color: Colors.purple,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Expanded(
-                child: ExpansionTile(
-                    title: Text(widget.brandName),
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.amber,
-                      child: Text(widget.categoryId.toString()),
+              Obx(() => Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
                     ),
-                    children: widget.listOfBrands
-                        .map((element) => Text(element.title))
-                        .toList()),
+                    child: (imagePath.isNotEmpty)
+                        ? Image.file(
+                            File(imagePath),
+                            height: 200,
+                            fit: BoxFit.cover,
+                          )
+                        : Center(
+                            child: Text('No Image Selected'),
+                          ),
+                  )),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Latitude:',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Obx(() => Text(
+                        locationParts.isNotEmpty ? locationParts[0] : '',
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                      )),
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Longitude:',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Obx(() => Text(
+                        locationParts.length > 1 ? locationParts[1] : '',
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center,
+                      )),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Name:',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 8),
+                  Text(
+                    name,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text('Back to Capture Information'),
+                style: ElevatedButton.styleFrom(primary: Colors.blue),
               ),
             ],
           ),
@@ -122,3 +117,4 @@ class _CategoryTileState extends State<CategoryTile> {
     );
   }
 }
+
